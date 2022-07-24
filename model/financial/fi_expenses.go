93ff48cfef87ce4edf3b60abbcca09ba20e2e2fd
tongxz96 <tongxz96@outlook.com>
2022-07-24 16:27:54 +0800

    fix: 支出管理时间类型格式修改

--- model/financial/fi_expenses.go
+++ model/financial/fi_expenses.go
@@ -3,22 +3,21 @@ package financial
 
 import (
 	"github.com/tongxz/xs-admin-vue/global"
-	"time"
 )
 
 // Expenses 结构体
 // 如果含有time.Time 请自行import time包
 type Expenses struct {
 	global.GVA_MODEL
-	ExpnDate   *time.Time `json:"expnDate" form:"expnDate" gorm:"column:expnDate;comment:收入日期;"`
-	Content    string     `json:"content" form:"content" gorm:"column:content;comment:支出内容说明;"`
-	Amount     *float64   `json:"amount" form:"amount" gorm:"column:amount;comment:支出金额;"`
-	Department string     `json:"department" form:"department" gorm:"column:department;comment:部门;"`
-	Type       *int       `json:"type" form:"type" gorm:"column:type;comment:支出类型;"`
-	Payment    *int       `json:"payment" form:"payment" gorm:"column:payment;comment:支付方式;"`
-	Executor   string     `json:"executor" form:"executor" gorm:"column:executor;comment:执行该操作的人员;"`
-	Invoice    *bool      `json:"invoice" form:"invoice" gorm:"column:invoice;comment:是否开票;"`
-	Note       string     `json:"note" form:"note" gorm:"column:note;comment:备注说明;"`
+	ExpnDate   string   `json:"expnDate" form:"expnDate" gorm:"type:data;column:expnDate;comment:收入日期;"`
+	Content    string   `json:"content" form:"content" gorm:"column:content;comment:支出内容说明;"`
+	Amount     *float64 `json:"amount" form:"amount" gorm:"column:amount;comment:支出金额;"`
+	Department string   `json:"department" form:"department" gorm:"column:department;comment:部门;"`
+	Type       *int     `json:"type" form:"type" gorm:"column:type;comment:支出类型;"`
+	Payment    *int     `json:"payment" form:"payment" gorm:"column:payment;comment:支付方式;"`
+	Executor   string   `json:"executor" form:"executor" gorm:"column:executor;comment:执行该操作的人员;"`
+	Invoice    *bool    `json:"invoice" form:"invoice" gorm:"column:invoice;comment:是否开票;"`
+	Note       string   `json:"note" form:"note" gorm:"column:note;comment:备注说明;"`
 }
 
 // TableName Expenses 表名
